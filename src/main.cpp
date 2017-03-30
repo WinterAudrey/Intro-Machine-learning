c7328b5a46804b8b5835e70b0abbce377d0202a8
Your Name <you@example.com>
2017-03-30 18:13:57 +0200

    ok

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 
 #include "jpg.h"
 #include "mnist.h"
+#include <limits.h>
 
 float dist_sq(float* v1, float* v2) {
 	float d=0;
@@ -8,6 +9,15 @@ float dist_sq(float* v1, float* v2) {
 		d+=(v1[i]-v2[i])*(v1[i]-v2[i]);
 	}
 	return d;
+}
+
+float linear_classifier(float*w,float*x){
+	float d=0;
+	for(int i=0; i<784; i++){
+		d += w[i]*x[i];
+	}
+	if(d>=0) return 1;
+	else return 0;
 }
 
 
@@ -19,25 +29,26 @@ int main()
 	float* labels = read_labels("train-labels.idx1-ubyte");
 	//les 10000 images dont il faut deviner le label
 	float** test_images = read_mnist("t10k-images.idx3-ubyte");
+	//les labels des 10000 images pour tester
+	float* test_labels = read_labels("t10k-labels.idx1-ubyte");
+	float*w= new float[784];
+	for(int i=0; i<784; i++){
+		w[i]=(float)rand()*2/INT_MAX-1;
+	}
+	float E=0;
 
     for(int i=0; i<10000; i++) {
 		printf("%u\n",i);
 		float mind=-1;
-		int NN; // le plus proche voisin (nearest neighboor)
-		for(int j=0; j<60000; j++){
-			// but: comparer image i à image connu j
-				float d = dist_sq(test_images[i], data[j]);
-				if(d<mind || mind==-1) {
-					mind=d;
-					NN=j;
-				}
-		}
-		// a ce stade : NN contient le numero du plus proche voisin.
-		// mais attention, nous n'avons pas encore son label
-		int inference = labels[NN];
+		int inference=linear_classifier(w, test_images[i]);
 		// inference contient maintenant le label associé à l'image connue
-        
+		
       	save_jpg(test_images[i], 28, 28, "%u/%u.jpg", inference, i);
+	
+		// if(inference!=test_labels[i]){
+		//	E++;
+		//}
+		//printf("Erreur= %0.2f\n",E*100/i);
     }
     return 0;
 }
